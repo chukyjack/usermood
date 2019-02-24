899f2424f2cc25a658932c28a53849f46faf3abd
chukyjack <chukyjack@gmail.com>
2019-02-24 12:00:07 -0800

    fixed migrations

--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,10 @@
 from django.urls import path, include
-<<<<<<< HEAD
-=======
-from django.contrib.auth.decorators import login_required
->>>>>>> origin/master
+
 from . import views
 
 urlpatterns = [
     path('mood/', views.ListMood.as_view()),
-<<<<<<< HEAD
     path('rest-auth/', include('rest_auth.urls')),
     path('api-auth/', include('rest_framework.urls'))
-=======
-    # path('mood/', views.mood_list, name='mood_list'),
-    path('rest-auth/', include('rest_auth.urls')),
-    # url(r'^tasks/(?P<pk>[0-9]+)$', views.task_detail, name='task_detail'),
->>>>>>> origin/master
+
 ]
--- users/admin.py
+++ users/admin.py
@@ -3,9 +3,4 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 # Register your models here.
-<<<<<<< HEAD
 admin.site.register(CustomUser,UserAdmin)
-=======
-# admin.site.unregister(User)
-admin.site.register(CustomUser,UserAdmin)
->>>>>>> origin/master
